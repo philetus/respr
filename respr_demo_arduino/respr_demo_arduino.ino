51ccbf7df11433c907258271bf977464d4b97ce8
michael philetus weller <philetus@gmail.com>
2016-03-12 17:21:45 -0800

    first working demo  * moved bluetooth connect test into loop to allow repeated    connect/disconnect from app  * moved defines into config

--- respr_demo_arduino/respr_demo_arduino.ino
+++ respr_demo_arduino/respr_demo_arduino.ino
@@ -1,16 +1,10 @@
-/*********************************************************************
- This is an example for our nRF51822 based Bluefruit LE modules
-
- Pick one up today in the adafruit shop!
-
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
+//*********************************************************************
+// respr demo
+//
+// polls resistance of conductive rubber band on analog pin 0 and
+// transmits value over bluetooth connection
+//
+//*********************************************************************
 
 #include <Arduino.h>
 #include <SPI.h>
@@ -21,74 +15,15 @@
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
-
 #include "BluefruitConfig.h"
 
-/*=========================================================================
-    APPLICATION SETTINGS
-
-    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
-   
-                              Enabling this will put your Bluefruit LE module
-                              in a 'known good' state and clear any config
-                              data set in previous sketches or projects, so
-                              running this at least once is a good idea.
-   
-                              When deploying your project, however, you will
-                              want to disable factory reset by setting this
-                              value to 0.  If you are making changes to your
-                              Bluefruit LE device via AT commands, and those
-                              changes aren't persisting across resets, this
-                              is the reason why.  Factory reset will erase
-                              the non-volatile memory where config data is
-                              stored, setting it back to factory default
-                              values.
-       
-                              Some sketches that require you to bond to a
-                              central device (HID mouse, keyboard, etc.)
-                              won't work at all with this feature enabled
-                              since the factory reset will clear all of the
-                              bonding data stored on the chip, meaning the
-                              central device won't be able to reconnect.
-    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
-    MODE_LED_BEHAVIOUR        LED activity, valid options are
-                              "DISABLE" or "MODE" or "BLEUART" or
-                              "HWUART"  or "SPI"  or "MANUAL"
-    -----------------------------------------------------------------------*/
-    #define FACTORYRESET_ENABLE         1
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-    #define MODE_LED_BEHAVIOUR          "MODE"
-/*=========================================================================*/
-
-// What pin to connect the breath sensor to
-#define SENSORPIN A0 
-#define TX_DELAY 100
+#define SENSORPIN A0 // breath sensor pin
+#define TX_DELAY 100 // sensor polling delay
 
 // Create the bluefruit object, either software serial...uncomment these lines
-/*
-SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
-
-Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
-                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
-*/
-
-/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
-//Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
-
-/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+// ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
-/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
-//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
-//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
-//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-
-
-/* // A small helper
-void error(const __FlashStringHelper*err) {
-  Serial.println(err);
-  while (1);
-} */
 
 /**************************************************************************/
 /*!
@@ -98,35 +33,8 @@ void error(const __FlashStringHelper*err) {
 /**************************************************************************/
 void setup(void)
 {
-  delay(500);
-
-  /* Initialise the module */
+  /* Initialise the ble module */
   ble.begin(VERBOSE_MODE);
-
-  /* Perform a factory reset to make sure everything is in a known state */
-  if (FACTORYRESET_ENABLE) ble.factoryReset();
-
-  /* Disable command echo from Bluefruit */
-  ble.echo(false);
-  ble.verbose(false);  // debug info is a little annoying after this point!
-
-  /* Wait for connection */
-  while (!ble.isConnected()) {
-      delay(500);
-  }
-
-  // LED Activity command is only supported from 0.6.6
-  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
-  {
-    // Change Mode LED Activity
-    //Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
-    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
-  }
-
-  // Set module to DATA mode
-  //Serial.println( F("Switching to DATA mode!") );
-  ble.setMode(BLUEFRUIT_MODE_DATA);
-
 }
 
 /**************************************************************************/
@@ -137,48 +45,37 @@ void setup(void)
 void loop(void)
 {
   float reading;
+
+  initBle(); // make sure ble is connected before attempting read
+  
   reading = analogRead(SENSORPIN);
-  //Serial.println(reading);
   ble.println(reading);
   delay(TX_DELAY);
-  
-  /*
-  // Check for user input
-  char n, inputs[BUFSIZE+1];
-
- reading serial input from the arduino serial montitor
-  if (Serial.available())
-  {
-    n = Serial.readBytes(inputs, BUFSIZE);
-    inputs[n] = 0;
-    // Send characters to Bluefruit
-    Serial.print("Sending: ");
-    Serial.print(inputs);
-
-    // Send input data to host via Bluefruit
-    ble.print(inputs);
-  }
-
-  // Echo received data
-  while ( ble.available() )
-  {
-    int c = ble.read();
-
-    Serial.print((char)c);
-
-    // Hex output too, helps w/debugging!
-    Serial.print(" [0x");
-    if (c <= 0xF) Serial.print(F("0"));
-    Serial.print(c, HEX);
-    Serial.print("] ");
-  }*/
-  
 }
 
+void initBle() {
+  if (ble.isConnected()) return;
 
+  /* Wait for connection */
+  while (!ble.isConnected()) {
+      delay(500);
+  }
 
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
+  /* Perform a factory reset to make sure everything is in a known state */
+  if (FACTORYRESET_ENABLE) ble.factoryReset();
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set module to DATA mode
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+}
+
